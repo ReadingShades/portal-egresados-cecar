1ee35cb395dcdb95f41714dd101499a16abae8c4
ReadingShades <42413498+ReadingShades@users.noreply.github.com>
2020-11-25 16:16:07 -0500

    ajusted a modelo mvc
    movi los scripts probados de views a su carpeta correspondiente

--- index.php
+++ index.php
@@ -1,169 +1,169 @@
-<?php include_once("./header.view.php"); ?>
+<?php include_once("./views/header.view.php"); ?>
 
 <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-3">
-					<img alt="Bootstrap Image Preview" src="./img/Logo Cecar.jpg">
-				</div>
-				<div class="col-md-6">
-					<nav>
-						<ul class="pagination">
-							<li class="page-item">
-								<a class="page-link" href="https://cecarvirtual.cecar.edu.co/">CECAR VIRTUAL</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="https://aldea.cecar.edu.co/Login">ALDEA</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="https://www.cecar.edu.co/graduados">GRADUDADOS</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="https://www.cecar.edu.co/punto-bvc">PUNTO BVC</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="https://www.cecar.edu.co/icfes-saber-pro">SABER PRO</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
-				<div class="col-md-3">
-					<nav>
-						<ol class="breadcrumb">
-							<li class="breadcrumb-item">
-								<a href="#">Home</a>
-							</li>
-							<li class="breadcrumb-item">
-								<a href="#">Library</a>
-							</li>
-							<li class="breadcrumb-item active">
-								Data
-							</li>
-						</ol>
-					</nav>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-3">
+                    <img alt="Bootstrap Image Preview" src="./rsc/img/Logo Cecar.jpg">
+                </div>
+                <div class="col-md-6">
+                    <nav>
+                        <ul class="pagination">
+                            <li class="page-item">
+                                <a class="page-link" href="https://cecarvirtual.cecar.edu.co/">CECAR VIRTUAL</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="https://aldea.cecar.edu.co/Login">ALDEA</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="https://www.cecar.edu.co/graduados">GRADUDADOS</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="https://www.cecar.edu.co/punto-bvc">PUNTO BVC</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="https://www.cecar.edu.co/icfes-saber-pro">SABER PRO</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-md-3">
+                    <nav>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="breadcrumb-item">
+                                <a href="#">Library</a>
+                            </li>
+                            <li class="breadcrumb-item active">
+                                Data
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-							<span class="navbar-toggler-icon"></span>
-						</button> <a class="navbar-brand" href="#">Brand</a>
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-							<ul class="navbar-nav">
-								<li class="nav-item active">
-									<a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="#">Link</a>
-								</li>
-								<li class="nav-item dropdown">
-									<a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
-									<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-										<a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-										<div class="dropdown-divider">
-										</div> <a class="dropdown-item" href="#">Separated link</a>
-									</div>
-								</li>
-							</ul>
-							<form class="form-inline">
-								<input class="form-control mr-sm-2" type="text">
-								<button class="btn btn-primary my-2 my-sm-0" type="submit">
-									Search
-								</button>
-							</form>
-							<ul class="navbar-nav ml-md-auto">
-								<li class="nav-item active">
-									<a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
-								</li>
-								<li class="nav-item dropdown">
-									<a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
-									<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-										<a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-										<div class="dropdown-divider">
-										</div> <a class="dropdown-item" href="#">Separated link</a>
-									</div>
-								</li>
-							</ul>
-						</div>
-					</nav>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<div class="media">
-						<img class="mr-3" alt="Bootstrap Media Preview" src="https://www.layoutit.com/img/sports-q-c-64-64-8.jpg">
-						<div class="media-body">
-							<h5 class="mt-0">
-								Nested media heading
-							</h5> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-							<div class="media mt-3">
-								<a class="pr-3" href="#"><img alt="Bootstrap Media Another Preview" src="https://www.layoutit.com/img/sports-q-c-64-64-2.jpg"></a>
-								<div class="media-body">
-									<h5 class="mt-0">
-										Nested media heading
-									</h5> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<div class="row">
-						<div class="col-md-3">
-							<div class="card">
-								<h5 class="card-header">
-									Card title
-								</h5>
-								<div class="card-body">
-									<p class="card-text">
-										Card content
-									</p>
-								</div>
-								<div class="card-footer">
-									Card footer
-								</div>
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="card">
-								<h5 class="card-header">
-									Card title
-								</h5>
-								<div class="card-body">
-									<p class="card-text">
-										Card content
-									</p>
-								</div>
-								<div class="card-footer">
-									Card footer
-								</div>
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="card">
-								<h5 class="card-header">
-									Card title
-								</h5>
-								<div class="card-body">
-									<p class="card-text">
-										Card content
-									</p>
-								</div>
-								<div class="card-footer">
-									Card footer
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="navbar-toggler-icon"></span>
+                        </button> <a class="navbar-brand" href="#">Brand</a>
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+                                        <div class="dropdown-divider">
+                                        </div> <a class="dropdown-item" href="#">Separated link</a>
+                                    </div>
+                                </li>
+                            </ul>
+                            <form class="form-inline">
+                                <input class="form-control mr-sm-2" type="text">
+                                <button class="btn btn-primary my-2 my-sm-0" type="submit">
+                                    Search
+                                </button>
+                            </form>
+                            <ul class="navbar-nav ml-md-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                        <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+                                        <div class="dropdown-divider">
+                                        </div> <a class="dropdown-item" href="#">Separated link</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="media">
+                        <img class="mr-3" alt="Bootstrap Media Preview" src="https://www.layoutit.com/img/sports-q-c-64-64-8.jpg">
+                        <div class="media-body">
+                            <h5 class="mt-0">
+                                Nested media heading
+                            </h5> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+                            <div class="media mt-3">
+                                <a class="pr-3" href="#"><img alt="Bootstrap Media Another Preview" src="https://www.layoutit.com/img/sports-q-c-64-64-2.jpg"></a>
+                                <div class="media-body">
+                                    <h5 class="mt-0">
+                                        Nested media heading
+                                    </h5> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <h5 class="card-header">
+                                    Card title
+                                </h5>
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        Card content
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    Card footer
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card">
+                                <h5 class="card-header">
+                                    Card title
+                                </h5>
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        Card content
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    Card footer
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card">
+                                <h5 class="card-header">
+                                    Card title
+                                </h5>
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        Card content
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    Card footer
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
-<?php include_once("./footer.view.php"); ?>
+<?php include_once("./views/footer.view.php"); ?>
---
+++ views/footer.view.php
@@ -0,0 +1,6 @@
+
+    <script src="./rsc/js/jquery.min.js"></script>
+    <script src="./rsc/js/bootstrap.min.js"></script>
+    <script src="./rsc/js/scripts.js"></script>
+  </body>
+</html>
