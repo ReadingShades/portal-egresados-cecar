97ecf52c2f0ccc72ed29ad06d4cbae6b793fdd35
Desgun <hernanvelasquez205@gmail.com>
2020-11-25 10:01:49 -0500

    Carpetas mvc
    Coloque la carpeta de modelo, vista, controlador. En el modelo hice la conexción a la base de datos, pasando los parametros de la conectvars

---
+++ model/conexcion.php
@@ -0,0 +1,36 @@
+<?php
+
+   // require_once $_SERVER['DOCUMENT_ROOT'] . './Model/connectvars.php';
+    require_once('./Model/connectvars.php');
+    class Conectar_Databases{
+
+        private $hostname = DB_HOST;
+        private $database = DB_NAME;
+        private $user = DB_USER;
+        private $password = DB_PASSWORD;
+        private $charset = DB_CHARSET;
+        private $conexion;
+
+        public function getconection(){
+            try{
+                $opciones = array(
+                    PDO::ATTR_PERSISTENT=>true,
+                    PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
+                );
+
+                $this-> conexion = new PDO('mysql:host='. $this-> hostname .'; dbname='.$this->database,
+                                            $this-> user, $this-> password, $opciones);
+                
+                $this-> conexion-> exec('SET NAME'. $this->charset . '');
+                
+                echo 'Conexión Exitosa';
+            }catch (PDOException $error){
+                echo "! ERROR: ¡ " . $error-> getMenssage();
+                die();
+            }
+
+            return $this-> conexion;
+        }
+    }
+
+?>
