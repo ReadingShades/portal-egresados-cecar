ab48999dc8ef7ab897d1d0e8767674db733ca659
ReadingShades <42413498+ReadingShades@users.noreply.github.com>
2020-11-28 16:45:56 -0500

    fixed db issues
    fixed postdb
    fixed twitter table trigger

--- model/insertPostDB.php
+++ model/insertPostDB.php
@@ -19,27 +19,29 @@ print_r($_POST);
 
     ?>
 </table>
+
 <?php
 if (isset($_POST)) {
     require_once("./connectvars.php");
     // Connect to the database
     $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     // 
-    $UserID = mysqli_escape_string($dbc, trim($_POST['user-id']));
-    $UserName = mysqli_escape_string($dbc, trim($_POST['Nombre']));
+    $UserID = mysqli_escape_string($dbc, trim($_POST['user-id']));    
     $pubTitulo = mysqli_escape_string($dbc, trim($_POST['publicacionTitulo']));
     $pubTipo = mysqli_escape_string($dbc, trim($_POST['publicacionTipo']));
     $pubContent = mysqli_escape_string($dbc, trim($_POST['publicacionCuerpo']));
+    $pubFechapub = date("Y-m-d");
     $pubfechaInicio = mysqli_escape_string($dbc, trim($_POST['pubFechaInit']));
     $pubfechaExpire = mysqli_escape_string($dbc, trim($_POST['pubFechaExp']));
 
-    $query = "INSERT INTO oferta (ofertaID,userID,Titulo,Contenido,FechaPub,FechaInicio,FechaExp,TipoOferta) 
-    VALUES ('$UserID','$pubTipo','$UserName','$apel','$password','$email','$tuser')";
-    echo $query;
-    $result = mysqli_query($dbc, $query);
+    $query = "INSERT INTO `oferta` (`ofertaID`, `userID`, `Titulo`, `Contenido`, `FechaPub`, `FechaInicio`, `FechaExp`, `TipoOferta`) 
+    VALUES (NULL, '$UserID','$pubTitulo','$pubContent','$pubFechapub','$pubfechaInicio','$pubfechaExpire','$pubTipo')";
+    echo "\n".$query.PHP_EOL;
+    $result = mysqli_query($dbc, $query) or die(mysqli_error($dbc));
+    echo $result;
     mysqli_close($dbc);
 
-    echo "Creacion de nuevo usuario exitoso.";
+    echo "\n Creacion de publicacion exitoso.";
 } else {
     echo "Error.";
 }
